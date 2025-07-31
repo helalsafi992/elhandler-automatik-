7862c6f5472b120311f3e450065a7edfbf116f9e
helalsafi992 <helalsafi992@gmail.com>
2025-07-31 16:05:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def run_model(zone):
 
     # Gyldige kurver til EQ v0.14.6 og timeseries.load()
     curve_map = {
-        "Wind": "Forecasts DK1 Wind Power MWh/h",
-        "Solar": "Forecasts DK1 Solar Power MWh/h",
-        "Consumption": "Forecasts DK1 Consumption MWh/h",
-        "ResidualLoad": "Forecasts DK1 Residual Load MWh/h",
-        "Temperature": "Forecasts DK1 Temperature °C"
-    }
+    "Wind": "Forecasts DK1 Wind Power MWh/h",
+    "Solar": "Forecasts DK1 Solar Power MWh/h",
+    "Consumption": "Forecasts DK1 Consumption MWh/h",
+    "ResidualLoad": "Forecasts DK1 Residual Load MWh/h",
+    "Temperature": "Forecasts DK1 Temperature °C"
+}
 
     for var, curve in curve_map.items():
         fc = get_forecast(curve)
