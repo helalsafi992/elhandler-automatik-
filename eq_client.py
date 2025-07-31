153ca73a811f7eda28d621df773d8dacd7724e0f
helalsafi992 <helalsafi992@gmail.com>
2025-07-31 14:49:27 +0200

    Update eq_client.py

--- eq_client.py
+++ eq_client.py
@@ -9,8 +9,8 @@ eq = EnergyQuantified(api_key=API_KEY)
 def list_eq_curves():
     curves = eq.metadata.curves(q="dk1 forecast")
     for c in curves:
-        if c.type == "TIMESERIES":
-            print(c.name)
+        if c.type in ["TIMESERIES", "SCENARIO_TIMESERIES"]:
+            print(f"{c.name} [{c.type}]")
 
 def get_forecast(curve_name: str):
     target_date = date.today() + timedelta(days=2)
