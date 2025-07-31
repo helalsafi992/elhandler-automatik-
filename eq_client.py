fd0916a0002d158f8baddd6b7c08112b3612faea
helalsafi992 <helalsafi992@gmail.com>
2025-07-31 15:55:45 +0200

    Update eq_client.py

--- eq_client.py
+++ eq_client.py
@@ -7,8 +7,13 @@ API_KEY = os.getenv("EQ_API_KEY")
 eq = EnergyQuantified(api_key=API_KEY)
 
 def get_forecast(curve_name: str):
-    instance = eq.instances.get_latest(curve_name)
-    df = instance.to_dataframe()
+    target_date = date.today() + timedelta(days=2)
+    timeseries = eq.timeseries.load(
+        curve=curve_name,
+        begin=target_date,
+        end=target_date
+    )
+    df = timeseries.to_dataframe()
     df = df.resample("1H").mean().reset_index()
     df["Hour"] = df["date"].dt.hour
     return df[["Hour", "value"]]
