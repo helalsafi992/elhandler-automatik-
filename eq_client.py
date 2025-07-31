d1d325262d903fea8094d80cc6c8a64e216846d8
helalsafi992 <helalsafi992@gmail.com>
2025-07-31 12:42:57 +0200

    Create eq_client.py

---
+++ eq_client.py
@@ -0,0 +1,18 @@
+from energyquantified import EnergyQuantified
+import pandas as pd
+from datetime import date, timedelta
+
+API_KEY = os.getenv("EQ_API_KEY")
+eq = EnergyQuantified(api_key=API_KEY)
+
+def get_forecast(curve_name: str, zone="DK1"):
+    target_date = date.today() + timedelta(days=2)
+    timeseries = eq.timeseries.load(
+        curve=f"{zone} {curve_name} MWh/h 15min Forecast.4",
+        begin=target_date,
+        end=target_date
+    )
+    df = timeseries.to_dataframe()
+    df = df.resample("1H").mean().reset_index()
+    df["Hour"] = df["date"].dt.hour
+    return df[["Hour", "value"]].rename(columns={"value": curve_name})
